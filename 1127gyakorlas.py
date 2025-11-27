4975abfd01ef57c063c3ce16d4a423a93da27cde
kissgya <kissgy@kkszki.hu>
2025-11-27 16:54:18 +0100

    Task 2 is done

--- 1127gyakorlas.py
+++ 1127gyakorlas.py
@@ -3,3 +3,10 @@ szam2 = float(input("Adj meg egy másik számot: "))
 
 osszeg=szam1+szam2
 print("A két megadott szám összege: ", osszeg)
+
+
+szam = int(input("Adj meg egy számot: "))
+if szam % 2 == 0:
+    print("A megadott szám páros")
+else:
+    print("A megadott szám páratlan")
