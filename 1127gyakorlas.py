ccddbe165d4a9c156dabf55f2efc2f7d55934c10
kissgya <kissgy@kkszki.hu>
2025-11-27 16:59:43 +0100

    Task 3 is done

--- 1127gyakorlas.py
+++ 1127gyakorlas.py
@@ -10,3 +10,13 @@ if szam % 2 == 0:
     print("A megadott szám páros")
 else:
     print("A megadott szám páratlan")
+
+
+szam1 = float(input("Adj meg egy számot: "))
+szam2 = float(input("Adj meg egy másik számot: "))
+if szam1 > szam2:
+    print("A nagyobb szám:", szam1)
+elif szam2 > szam1:
+    print("A nagyobb szám:", szam2)
+else:
+    print("A két megadott szám egyenlő.")
